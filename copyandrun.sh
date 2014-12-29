51e4ea41d445991053384fdc775df672323e9d14
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2014-12-29 18:49:33 +0100

    tidy

--- copyandrun.sh
+++ copyandrun.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if [ ! -f mypi_url ]; then
+if [ ! -f mypi ]; then
 	echo 'Create a file named "mypi" with the user name and ip adress of your pi. Like pi@192.168.0.21'
 	false
 fi
 
-${mypi} = $(cat mypi)
+mypi=$(cat mypi)
 scp piservo.py ${mypi}:~/dev/piservo
 ssh ${mypi} 'cd ~/dev/piservo;sudo python piservo.py'
